a5aba049485479aaf2087e47ddae7f9e9f863396
sherlokonpony <sherlockonpony@gmail.com>
2024-09-15 16:58:30 +0300

    ДЗ - 4й урок.

---
+++ src/main/kotlin/lesson4/homeworklesson4.kt
@@ -0,0 +1,47 @@
+package com.sherlockonpony.amkkc.lesson4
+
+val myInt: Int = 42 //42 MAX_VALUE: Int = 2147483647 val MAX_VALUE: Short = 32767 MAX_VALUE: Byte = 127
+val myLong: Long = 98765432123456789L //98765432123456789L MAX_VALUE: Long = 9223372036854775807L
+val myFloat: Float = 23.45f //23.45f
+val myDouble: Double = 0.123456789//0.123456789
+val myString: String = "Kotlin & Java"//"Kotlin & Java"
+val isKotlinFun: Boolean = false //false
+val myChar: Char = 'c' //'c'
+val myInt2: Int = 500 //500
+val myLong2: Long = 4294967296L //4294967296L
+val myFloat2: Float = 18.0f // 18.0f
+val myDouble2: Double = -0.001 //-0.001
+val myString2: String = "OpenAI"//"OpenAI"
+“true” - синтаксическая ошибка в кавычках. если исправить кавычки, то//“true”
+val myString3: String = "true"
+если удалить кавычки и оставить без них, то
+val isKotlinFun2: Boolean = true
+val myChar2: Char = '9' //'9'
+val myInt3: Int = 2048 //2048
+val myLong3: Long = 10000000000L //10000000000L
+val myFloat3: Float = 5.75f //5.75f
+val myDouble3: Double = 1.414 //1.414
+val myString4: String = "Artificial Intelligence"//"Artificial Intelligence"
+val isKotlinFun3: Boolean = false //false
+val myChar3: Char = '@'//'@'
+val myInt4: Int = 1024 //1024
+val myLong4: Long = 1234567890123L//1234567890123L
+val myFloat4: Float = 10.01f//10.01f
+val myDouble4: Double = -273.15 //-273.15
+val myString5: String = "SpaceX"//"SpaceX"
+val isKotlinFun3: Boolean = true//true
+“🤯” - синтаксическая ошибка в кавычках. если исправить кавычки, то//“🤯”
+val myString6: String = "🤯"
+‘65535’- синтаксическая ошибка в кавычках. с одиночными кавычками идёт только ЧАР, но ЧАР это 1 символ, если без кавычек тогда//‘65535’
+val myInt5: Int = 65535
+val myLong5: Long = 72057594037927935L //72057594037927935L
+val myFloat5: Float = 2.71828f //2.71828f
+val myDouble5: Double = 101.0101 //101.0101
+val myString7: String = "Quantum Computing"//"Quantum Computing"
+val isKotlinFun4: Boolean = false //false
+val myChar4: Char = 'x' //'x'
+val myInt6: Int = 314//314
+val myLong6: Long = 123456789123456789L //123456789123456789L
+val myFloat6: Float = 6.626f//6.626f
+val myDouble6: Double = 0.007//0.007
+val myString8: String = "Android Studio"//"Android Studio"
