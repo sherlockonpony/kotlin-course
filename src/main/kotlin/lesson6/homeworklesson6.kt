77ef1c4ec382e775aabb06090284a799f4dd0f42
sherlokonpony <sherlockonpony@gmail.com>
2024-09-22 18:24:08 +0300

    ДЗ - 6й урок

--- src/main/kotlin/lesson6/homeworklesson6.kt
+++ src/main/kotlin/lesson6/homeworklesson6.kt
@@ -14,10 +14,10 @@ fun main (){
 
 fun seasonIndication(month: Int):String {
     return when (month) {
-        in 1..2 , 12 -> "Winter"
-        in 3..5 -> "Spring"
-        in 6..8 -> "Summer"
-        in 9..11 -> "Autumn"
+        in 1..2 , 12 -> "Зима"
+        in 3..5 -> "Весна"
+        in 6..8 -> "Лето"
+        in 9..11 -> "Весна"
         else -> "Invalid Error"
     }
 }
@@ -30,10 +30,10 @@ fun petAge(age: Int): Double {
 }
 fun vesselType(distance: Double): String {
     return if (distance < 1) {
-        "Foot"
+        "Пешком"
     } else if (distance in 1.0..5.0){
-        "Cycle"
-    }else "Car"
+        "Велосипед"
+    }else "Автотранспорт"
 }
 
 fun loyalty(buy: Double): Any {
@@ -46,12 +46,10 @@ fun loyalty(buy: Double): Any {
 }
 fun docType(type: String): String {
     return when(type) {
-        in "*txt","*doc","*docx" -> "WORD"
-        in "*xls","*xlsx" -> "EXCEL"
-        in "*ppt", "*pptx" -> "POWER POINT"
-        in "*jpeg","*png","*bmp","*gif" -> "PICS"
-        in "*avi", "*mov" -> "MOVIE"
-        else -> "Invalid Error"
+        in "txt","doc" -> "Текстовый документ"
+        in "jpeg","png" -> "Изображение"
+        in "xls" -> "Таблица"
+        else -> "Неизвестный формат"
     }
 }
 fun tempConv(value: Int, unit: String): String {
@@ -64,26 +62,23 @@ fun tempConv(value: Int, unit: String): String {
 
 fun clothesChoose(temp: Int): String {
     return when(temp) {
-        in -50..-15 -> "Very cold. Hardwinter clothes"
-        in -14..4 -> "Winter clothes"
-        in 4..16 -> "Autumn clothes"
+        in -30..-1 -> "куртка и шапка"
+        in 0..15 -> "ветровка"
+        in 16..35 -> "футболка и шорты"
         in 17..28 -> "Summer clothes"
-        in 29..40 -> "Naked"
-        else -> "You've left the Earth"
+        else -> "Рекомендую не выходить из дома"
     }
 }
 
 fun movieRank(age: Int): String {
     return if (age < 0 || age > 100) {
         "Неверное значение"
-    } else if (age in 0..6) {
-        "Small kid"
-    } else if (age in 7..12) {
-        "Child"
-    } else if (age in 13..18) {
-        "Teenager"
+    } else if (age in 0..11) {
+        "Детские"
+    } else if (age in 12..17) {
+        "Подростковые"
     }        else {
-        "All movies"
+        "18+"
     }
 
 }
